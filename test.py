afbe054179f0380e48699b6ef221f8cbcaad4f8a
hmkhoi2701 <hominhkhoi2701@gmail.com>
2021-11-10 19:50:00 +0700

    Update test.py
    Test file can now test:
    + Create blocks and maps
    + Moving blocks
    + Checking valid moves

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 import Map_and_Block
 
 a = Map_and_Block.Block(1, 1, 'vertical', 3, 1)
-b = Map_and_Block.Map()
-b.add_block(a)
-print(b, '\n')
-b.move_block(a, 2, 1)
-print(b)
+b = Map_and_Block.Block(0, 5, 'horizontal', 2, 2)
+m = Map_and_Block.Map()
+
+m.add_block(a)
+m.add_block(b)
+
+print(m,'\n')
+print('a can move', m.possible_move(a),'\n')
+print('b can move', m.possible_move(b),'\n')
+
+m.move_block(b,2,1)
+
+print(m,'\n')
+print('a can move', m.possible_move(a),'\n')
+print('b can move', m.possible_move(b),'\n')
+
+m.move_block(a,2,1)
+
+print(m,'\n')
+print('a can move', m.possible_move(a),'\n')
+print('b can move', m.possible_move(b),'\n')
