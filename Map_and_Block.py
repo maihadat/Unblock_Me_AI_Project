5f673921e276ecc4207b000266dd46c4c66fafff
hmkhoi2701 <hominhkhoi2701@gmail.com>
2021-12-02 08:09:17 +0700

    Update 2 files
    Map_and_Block.py and test.py temporarily updated. Changes will come shortly later.

--- Map_and_Block.py
+++ Map_and_Block.py
@@ -4,6 +4,7 @@ import numpy as np
 class Map:
     def __init__(self):
         self.map = np.array([[0 for i in range(6)] for i in range(6)])
+        self.blocks = Block.All_Blocks
 
     def __str__(self):
         return str(self.map)
@@ -37,24 +38,83 @@ class Map:
             for i in range(length):
                 self.map[y + i][x] = 0
 
-    def move_block(self, blk, step, direct):
-        Map.delete_block(self, blk)
-        blk.move(step, direct)
-        Map.add_block(self, blk)
+    def move_block(self, tup):
+        Map.delete_block(self, tup[0])
+        tup[0].move(tup[1], tup[2])
+        Map.add_block(self, tup[0])
 
+    def possible_move(self, blk):
+        x, y, direction, length = (
+            blk.start_point_x,
+            blk.start_point_y,
+            blk.direction,
+            blk.length,
+        )
+        move_list = []
+        if direction == "vertical":
+            if y == 0:
+                up = 0
+            else:
+                for up in range(1, y + 1):
+                    if self.map[y - up][x] != 0:
+                        up -= 1
+                        break
+                    else:
+                        move_list.append((blk, up, -1))    
+            if y + length == 6:
+                down = 0
+            else:
+                for down in range(1, 7 - y - length):
+                    if self.map[y + length + down - 1][x] != 0:
+                        down -= 1
+                        break
+                    else:
+                        move_list.append((blk, down, +1))
+
+        if direction == "horizontal":
+            if x == 0:
+                left = 0
+            else:
+                for left in range(1, x + 1):
+                    if self.map[y][x - left] != 0:
+                        left -= 1
+                        break
+                    else:
+                        move_list.append((blk, left, -1))
+            if x + length == 6:
+                right = 0
+            else:
+                for right in range(1, 7 - x - length):
+                    if self.map[y][x + length + right - 1] != 0:
+                        right -= 1
+                        break
+                    else:
+                        move_list.append((blk, right, +1))
+        return move_list
+    
+    def All_Moves(self):
+        All_Moves_list = []
+        for blk in Block.All_Blocks:
+            All_Moves_list += Map.possible_move(self,blk)
+        return All_Moves_list
 
 class Block:
     """
     x range from 0 to 5
     y range from 0 to 5
     """
+    All_Blocks = []
+    index = 1
 
-    def __init__(self, x, y, direction, length, index):
+    def __init__(self, x, y, direction, length):
         self.start_point_x = x
         self.start_point_y = y
         self.direction = direction
         self.length = length
-        self.indx = index
+        self.indx = Block.index
+        Block.All_Blocks.append(self)
+        Block.index += 1
+        
 
     def move(self, step, direct):
         """
--- test.py
+++ test.py
@@ -1,24 +1,29 @@
+from numpy import Inf
 import Map_and_Block
 
-a = Map_and_Block.Block(1, 1, 'vertical', 3, 1)
-b = Map_and_Block.Block(0, 5, 'horizontal', 2, 2)
+a = Map_and_Block.Block(5, 1, 'vertical', 3)
+b = Map_and_Block.Block(0, 5, 'horizontal', 2)
+X = Map_and_Block.Block(0, 2, 'horizontal', 2)
+
 m = Map_and_Block.Map()
 
-m.add_block(a)
-m.add_block(b)
+def Goal_state():
+    start = X.start_point_x
+    if sum(m.map[2][start:6]) == (X.index-1)*2:
+        return True
+    return False
+
+for blk in m.blocks:
+    Map_and_Block.Map.add_block(m, blk)
+
 
 print(m,'\n')
-print('a can move', m.possible_move(a),'\n')
-print('b can move', m.possible_move(b),'\n')
+print ('Goal state', Goal_state(),'\n')
 
-m.move_block(b,2,1)
-
+m.move_block (Map_and_Block.Map.All_Moves(m)[4])
 print(m,'\n')
-print('a can move', m.possible_move(a),'\n')
-print('b can move', m.possible_move(b),'\n')
+print ('Goal state', Goal_state(),'\n')
 
-m.move_block(a,2,1)
-
+m.move_block (Map_and_Block.Map.All_Moves(m)[2])
 print(m,'\n')
-print('a can move', m.possible_move(a),'\n')
-print('b can move', m.possible_move(b),'\n')
+print ('Goal state', Goal_state(),'\n')
