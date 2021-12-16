6db01a3f629253cac244682db245ca3e1fd69763
hmkhoi2701 <hominhkhoi2701@gmail.com>
2021-12-16 21:38:32 +0700

    Update ids.py
    delete old notes

--- ids.py
+++ ids.py
@@ -1,7 +1,7 @@
 import Map_and_Block
-# a = Map_and_Block.Block(5, 1, 'vertical', 3)
-# b = Map_and_Block.Block(0, 5, 'horizontal', 2)
-# X = Map_and_Block.Block(0, 2, 'horizontal', 2)
+import copy
+import time
+
 a = Map_and_Block.Block(2, 1, 'vertical', 2)
 b = Map_and_Block.Block(2, 3, 'vertical', 2)
 c = Map_and_Block.Block(1, 4, 'vertical', 2)
@@ -10,40 +10,14 @@ e = Map_and_Block.Block(4, 1, 'vertical', 3)
 f = Map_and_Block.Block(0, 3, 'horizontal', 2)
 g = Map_and_Block.Block(2, 5, 'horizontal', 2)
 X = Map_and_Block.Block(0, 2, 'horizontal', 2)
-# a = Map_and_Block.Block(0, 4, 'vertical', 2)
-# b = Map_and_Block.Block(1, 0, 'vertical', 2)
-# c = Map_and_Block.Block(2, 2, 'vertical', 2)
-# d = Map_and_Block.Block(4 ,1, 'vertical', 2)
-# e = Map_and_Block.Block(3, 4, 'horizontal', 3)
-# X = Map_and_Block.Block(0, 2, 'horizontal', 2)
-
 
 m = Map_and_Block.Map()
-
-# def Goal_state():
-#     start = X.start_point_x
-#     if sum(m.map[2][start:6]) == (X.index-1)*2:
-#         return True
-#     return False
 
 for blk in m.blocks:
     Map_and_Block.Map.add_block(m, blk)
 
-
 print(m,'\n')
-# print ('All available moves: ',Map_and_Block.Map.All_Moves(m))
-# print ('Goal state', Goal_state(),'\n')
 
-# m.move_block (Map_and_Block.Map.All_Moves(m)[4])
-# print(m,'\n')
-# print ('Goal state', Goal_state(),'\n')
-
-# m.move_block (Map_and_Block.Map.All_Moves(m)[2])
-# print(m,'\n')
-# print ('Goal state', Goal_state(),'\n')
-
-import copy
-import time
 # start = time.time()
 def goal_state(board, blk):
     start = blk.start_point_x
