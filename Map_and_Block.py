6a8ed5c940e9f9e037645f757b4a300b386a677b
hmkhoi2701 <hominhkhoi2701@gmail.com>
2021-12-17 15:17:22 +0700

    Update Map_and_Block.py
    New version of Map_and_Block

--- Map_and_Block.py
+++ Map_and_Block.py
@@ -1,10 +1,48 @@
 import numpy as np
+import copy
 
+index = 1
+class Block:
+    """
+    x range from 0 to 5
+    y range from 0 to 5
+    """ 
+    def __init__(self, x, y, direction, length):
+        global index
+        self.start_point_x = x
+        self.start_point_y = y
+        self.direction = direction
+        self.length = length
+        self.indx = index
+        index += 1          #when you initialize next item, its index will raise
+        
+    def __str__(self):
+        a = 'start_point_x = %d\
+        start_point_y = %d\
+        direction = %s\
+        length = %d\
+        index  = %d'\
+            % (int(self.start_point_x),\
+            int(self.start_point_y),\
+            self.direction,\
+            int(self.length),\
+            int(self.indx))
+        return a
+
+    def move(self, step, direct):
+        """
+        :param step: int
+        :param direct: -1/+1
+        :return: new block
+        """
+        if self.direction == "h":
+            self.start_point_x += step * direct
+        if self.direction == "v":
+            self.start_point_y += step * direct
 
 class Map:
     def __init__(self):
         self.map = np.array([[0 for i in range(6)] for i in range(6)])
-        self.blocks = Block.All_Blocks
 
     def __str__(self):
         return str(self.map)
@@ -17,41 +55,23 @@ class Map:
             blk.length,
             blk.indx,
         )
-        if direction == "horizontal":
+        if direction == "h":
             for i in range(length):
                 self.map[y][x + i] = index
-        if direction == "vertical":
+        if direction == "v":
             for i in range(length):
                 self.map[y + i][x] = index
-
-    def delete_block(self, blk):
-        x, y, direction, length = (
-            blk.start_point_x,
-            blk.start_point_y,
-            blk.direction,
-            blk.length,
-        )
-        if direction == "horizontal":
-            for i in range(length):
-                self.map[y][x + i] = 0
-        if direction == "vertical":
-            for i in range(length):
-                self.map[y + i][x] = 0
-
-    def move_block(self, tup):
-        Map.delete_block(self, tup[0])
-        tup[0].move(tup[1], tup[2])
-        Map.add_block(self, tup[0])
-
+        
     def possible_move(self, blk):
-        x, y, direction, length = (
+        x, y, direction, length, index = (
             blk.start_point_x,
             blk.start_point_y,
             blk.direction,
             blk.length,
+            blk.indx
         )
         move_list = []
-        if direction == "vertical":
+        if direction == "v":
             if y == 0:
                 up = 0
             else:
@@ -60,7 +80,8 @@ class Map:
                         up -= 1
                         break
                     else:
-                        move_list.append((blk, up, -1))    
+                        move_list.append((up, -1, index))    
+                        
             if y + length == 6:
                 down = 0
             else:
@@ -69,9 +90,9 @@ class Map:
                         down -= 1
                         break
                     else:
-                        move_list.append((blk, down, +1))
+                        move_list.append((down, +1, index))
 
-        if direction == "horizontal":
+        if direction == "h":
             if x == 0:
                 left = 0
             else:
@@ -80,7 +101,7 @@ class Map:
                         left -= 1
                         break
                     else:
-                        move_list.append((blk, left, -1))
+                        move_list.append((left, -1, index))
             if x + length == 6:
                 right = 0
             else:
@@ -89,51 +110,38 @@ class Map:
                         right -= 1
                         break
                     else:
-                        move_list.append((blk, right, +1))
+                        move_list.append((right, +1, index))
         return move_list
+
+class State():
+    def __init__(self, AllBlocks):
+        self.AllBlocks = AllBlocks
+        self.GameMap = Map()
+        for block in self.AllBlocks:
+            self.GameMap.add_block(block)
+            
+    def GetMap(self):
+        return self.GameMap
     
-    def All_Moves(self):
+    def GetNextMoves(self):
         All_Moves_list = []
-        for blk in self.blocks:
-            All_Moves_list += Map.possible_move(self,blk)
+        a = self.GetMap()
+        for blk in self.AllBlocks:
+            All_Moves_list += a.possible_move(blk)
         return All_Moves_list
 
-class Block:
-    """
-    x range from 0 to 5
-    y range from 0 to 5
-    """
-    All_Blocks = []
-    index = 1
+    def Display(self):
+        return self.GameMap.__str__()
+    
+    def NextStates(self, move):
+        New = []
+        for block in self.AllBlocks:
+            TempBlock = copy.copy(block)
+            if TempBlock.indx == move[2]:
+                TempBlock.move(move[1], move[0])
+            New.append(TempBlock)
+        return State(New)
 
-    def __init__(self, x, y, direction, length):
-        self.start_point_x = x
-        self.start_point_y = y
-        self.direction = direction
-        self.length = length
-        self.indx = Block.index
-        Block.All_Blocks.append(self)
-        Block.index += 1
-        
-
-    def move(self, step, direct):
-        """
-        :param step: int
-        :param direct: -1/+1
-        :return: new block
-        """
-        if self.direction == "horizontal":
-            self.start_point_x += step * direct
-        if self.direction == "vertical":
-            self.start_point_y += step * direct
-
-
-'''
-   Khi gõ các thông tin của map lên file txt thì cần theo một số quy chuần:
-       + Các cột theo thứ tự từ trái qua phải lần lượt là start_x, start_y, kí tự đầu của direc, length
-       + Khi điền block cuối(cũng là block đỏ) thì ko đc enter xuống dòng
-    Tôi có để file test_map.txt làm mẫu để mọi người điền cho ko bị lỗi
-'''
 def read_input(file_name): # read a text file and return an instance of Map class
     m = Map()
     with open(file_name) as f:
@@ -146,12 +154,16 @@ def read_input(file_name): # read a text file and return an instance of Map clas
                 block = list_of_blocks[i][:-1].strip()
             x = int(block[0])
             y = int(block[2])
-            if block[4] == 'h':
-                direc = 'horizontal'
-            elif block[4] == 'v':
-                direc = 'vertical'
+            direc = block[4]
             length = int(block[-1])
             blk = Block(x, y, direc, length)
             m.add_block(blk)
     return m
 
+
+#test unit, remove ''' ''' to test
+'''
+index = 1
+to_solve = read_input('testcases/inp12.txt')
+print (to_solve)
+'''
